19f194e32f471e4ceff440c03fc66aac5f62d962
yctan1993 <yongchintan93@gmail.com>
2025-05-29 22:59:11 +0800

    Updated

--- minikube_start.sh
+++ minikube_start.sh
@@ -1,15 +1,25 @@
-# minikube start --kubernetes-version=v1.30.5
-kubectl create namespace istio-system
-# minikube image load istio/pilot:1.25.3
-# minikube image load istio/proxyv2:1.25.3
-# minikube image load istio/install-cni:1.25.3
-# minikube image load busybox:latest
-
-helm install istio-base -n istio-system /Users/yongchintan/Desktop/Test/istio/base 
-helm install istiod -n istio-system /Users/yongchintan/Desktop/Test/istio/istio-control/istio-discovery/ #--set meshConfig.outboundTrafficPolicy.mode=REGISTRY_ONLY
-helm install istio-ingressgateway  -n istio-system /Users/yongchintan/Desktop/Test/istio/gateway_ingress
-helm install istio-egressgateway  -n istio-system /Users/yongchintan/Desktop/Test/istio/gateway_egress
-helm install istio-cni  -n istio-system /Users/yongchintan/Desktop/Test/istio/istio-cni
+# helm install istio-base -n istio-system /Users/yongchintan/Desktop/Test/istio_test/charts/base 
+# helm install istiod -n istio-system /Users/yongchintan/Desktop/Test/istio_test/charts/istio-control/istio-discovery/ #--set meshConfig.outboundTrafficPolicy.mode=REGISTRY_ONLY
+# helm install istio-ingressgateway  -n istio-system /Users/yongchintan/Desktop/Test/istio_test/charts/gateway_ingress
+# helm install istio-egressgateway  -n istio-system /Users/yongchintan/Desktop/Test/istio_test/charts/gateway_egress
+# helm install istio-cni  -n istio-system /Users/yongchintan/Desktop/Test/istio_test/charts/istio-cni
 
 # kubectl apply -f test.yaml
-# kubectl create deployment test --image=busybox:latest -n test
+
+minikube start --kubernetes-version=v1.30.5
+
+kubectl create namespace istio-system
+minikube image load istio/pilot:1.25.3
+minikube image load istio/proxyv2:1.25.3
+minikube image load istio/install-cni:1.25.3
+minikube image load busybox:latest
+
+kubectl create namespace istio-system
+kubectl apply -f gitRepo.yaml
+kubectl apply -f helmReleaseBase.yaml
+kubectl apply -f helmReleaseIstiod.yaml
+sleep 5
+kubectl apply -f helmReleaseIngress.yaml
+kubectl apply -f helmReleaseEgress.yaml
+
+# kubectl apply -f test.yaml
--- minikube_stop.sh
+++ minikube_stop.sh
@@ -1,16 +1,22 @@
-# kubectl delete deployment test -n test
-
-helm delete istiod -n istio-system
-helm delete istio-base -n istio-system
-helm delete istio-ingressgateway -n istio-system
-helm delete istio-egressgateway -n istio-system
-helm delete istio-cni  -n istio-system
-kubectl delete namespace istio-system
-# kubectl delete namespace test
-kubectl delete crd $(kubectl get CustomResourceDefinition -A | grep "istio.io" | awk '{print $1}')
+# helm delete istiod -n istio-system
+# helm delete istio-base -n istio-system
+# helm delete istio-ingressgateway -n istio-system
+# helm delete istio-egressgateway -n istio-system
+# helm delete istio-cni  -n istio-system
+# kubectl delete namespace istio-system
+# kubectl delete crd $(kubectl get CustomResourceDefinition -A | grep "istio.io" | awk '{print $1}')
 # kubectl delete -f test.yaml
-
 # minikube stop
 # minikube delete --purge
+
+kubectl delete -f helmReleaseIngress.yaml
+kubectl delete -f helmReleaseEgress.yaml
+kubectl delete -f helmReleaseIstiod.yaml
+kubectl delete -f helmReleaseBase.yaml
+kubectl delete -f gitRepo.yaml
+kubectl delete crd $(kubectl get CustomResourceDefinition -A | grep "istio.io" | awk '{print $1}')
+kubectl delete namespace istio-system
+# kubectl delete -f test.yaml
+# minikube stop
 
 
